26e07cfc98eaac4a6b825d7d57e7c660239494e6
Ashreen121 <Ashreen121@users.noreply.github.com>
2021-02-15 18:36:21 +0400

    created superuser and registered Deadline model

--- spotdb/app/models.py
+++ spotdb/app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Deadlines(models.Model):
+class Deadline(models.Model):
     assignment = models.CharField(max_length=200)
     due_date = models.DateTimeField('date published')
 
