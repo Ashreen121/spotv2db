46f93fd1fa6facaec720f1856d607aba33b60154
ASzecsenyi <73595315+ASzecsenyi@users.noreply.github.com>
2021-03-03 14:23:26 +0100

    Fix

--- spotdb/app/views.py
+++ spotdb/app/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'app/index.php')
+    return render(request, 'app/index.html')
 
 def deadlines(request):
-    return render(request, 'app/deadlines.php')
+    return render(request, 'app/deadlines.html')
 
 
 
