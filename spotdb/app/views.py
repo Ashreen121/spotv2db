dc12172d53047d12c82f9143f4d4ffaf8c143cdb
Ashreen121 <Ashreen121@users.noreply.github.com>
2021-03-07 12:13:53 +0400

    tested rendering of database items onto html webpage

--- spotdb/app/views.py
+++ spotdb/app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from .models import Student
 
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
 
 def deadlines(request):
-    return render(request, 'app/deadlines.html')
+    obj = Student.objects.get(id=1)
+    context = {
+        'student_object1': obj
+    }
+    return render(request, 'app/deadlines.html', context)
 
 
 
