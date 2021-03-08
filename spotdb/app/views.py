70eb89b9a340bb0f820cf17aa1f532906cd40fee
Ashreen121 <Ashreen121@users.noreply.github.com>
2021-03-08 21:15:36 +0400

    Added some html templates to the templates folder

--- spotdb/app/views.py
+++ spotdb/app/views.py
@@ -6,11 +6,16 @@ def index(request):
     return render(request, 'app/index.html')
 
 def deadlines(request):
-    obj = Student.objects.get(id=1)
-    context = {
-        'student_object1': obj
-    }
-    return render(request, 'app/deadlines.html', context)
+    return render(request, 'app/deadlines.html')
+
+
+    # Example of using context to render out database items
+    # obj = Student.objects.get(id=1)
+    # context = {
+    #     'student_object1': obj
+    # }
+    # return render(request, 'app/deadlines.html', context)
+
 
 
 
